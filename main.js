b627ea7d6e7395351f46beaa5235daa9b17b4107
Busayo Dawodu <39763573+Phumbie@users.noreply.github.com>
2020-11-13 21:28:55 +0100

    little addition

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ console.log("hi");
 particlesJS.load("particle-js", "/particlesjs-config.json", function () {
   console.log("callback - particles.js config loaded");
 });
+
+$(function () {
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab("show");
+
+  $(".nav-tabs a").click(function (e) {
+    $(this).tab("show");
+    var scrollmem = $("body").scrollTop();
+    window.location.hash = this.hash;
+    $("html,body").scrollTop(scrollmem);
+  });
+});
