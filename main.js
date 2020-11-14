19ee107ec66b3d588ec63f4a2b617ff2e6f429ae
Busayo Dawodu <39763573+Phumbie@users.noreply.github.com>
2020-11-14 09:15:25 +0100

    finished other pages

--- main.js
+++ main.js
@@ -4,14 +4,36 @@ particlesJS.load("particle-js", "/particlesjs-config.json", function () {
   console.log("callback - particles.js config loaded");
 });
 
-$(function () {
-  var hash = window.location.hash;
-  hash && $('ul.nav a[href="' + hash + '"]').tab("show");
+const tabs = document.querySelectorAll(".tab");
 
-  $(".nav-tabs a").click(function (e) {
-    $(this).tab("show");
-    var scrollmem = $("body").scrollTop();
-    window.location.hash = this.hash;
-    $("html,body").scrollTop(scrollmem);
-  });
-});
+function tabify(tab) {
+  const tabList = tab.querySelector(".tab__list");
+  console.log("hello there");
+
+  if (tabList) {
+    const tabItems = [...tabList.children];
+    const tabContent = tab.querySelector(".tab__content");
+    const tabContentItems = [...tabContent.children];
+    let tabIndex = 0;
+
+    tabIndex = tabItems.findIndex((item, index) => {
+      return [...item.classList].indexOf("is--active") > -1;
+    });
+
+    tabIndex > -1 ? (tabIndex = tabIndex) : (tabIndex = 0);
+
+    function setTab(index) {
+      tabItems.forEach((x, index) => x.classList.remove("is--active"));
+      tabContentItems.forEach((x, index) => x.classList.remove("is--active"));
+
+      tabItems[index].classList.add("is--active");
+      tabContentItems[index].classList.add("is--active");
+    }
+
+    tabItems.forEach((x, index) => x.addEventListener("click", () => setTab(index)));
+    setTab(tabIndex);
+    tab.querySelectorAll(".tab").forEach((tabContent) => tabify(tabContent));
+  }
+}
+
+tabs.forEach(tabify);
